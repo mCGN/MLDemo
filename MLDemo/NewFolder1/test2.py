c9c4dd7b65db2769fa2a21350843c1cb63140374
CGN <p1398010785@163.com>
2019-06-24 00:44:33 +0800

    .

--- MLDemo/NewFolder1/test2.py
+++ MLDemo/NewFolder1/test2.py
@@ -3,36 +3,52 @@ from sklearn import datasets
 import math
 import numpy as np
 
-#输入是4维的向量（4），隐藏层3，输出是（3）
+#输入是4维的向量（4），隐藏层(2)，输出是（3）
 iris = datasets.load_iris()
-x = iris.data
-n = len(x)
+LL = iris.data
+n = len(LL)
 
+x = np.array(LL) #n*4
 y = np.zeros((n,3))
 for i in range(len(iris.target)):
     y[i][iris.target[i]] = 1
 
-W1 = np.ones((4,3))
-W2 = np.ones((3,3))
+W1 = np.ones((4,2))
+W2 = np.ones((2,3))
+b1 = np.ones((1,2))
+b2 = np.ones((1,3))
 
 def sigmod(z):
 	return 1 / (1 + np.exp(-z))
 
-def softmax(z):
-	return np.exp(z) / np.sum(np.exp(z))
+
+def ds(o):
+	return o*(1-o)
+
+rate = 0.01
+
+for i in range(1000):
+	a2 = sigmod(x.dot(W1) + b1)
+	a3 = sigmod(a2.dot(W2) + b2)
+	
+	#[1,0,0] - [0.1,0.5,0.5] = 2(1-0.1)
+	2*(y-a2)
+
+
+
+def predict(it):
+	a1 = sigmod(it.dot(W1) + b1)
+	a2 = sigmod(a1.dot(W2) + b2)
+	return a2
+
+print(predict(x))
+
 
 def propagation():
-	a1 = sigmod(x.dot(W1))
-	return softmax(a1.dot(W2))
+	pass
 
 def backPropagation():
 
 	pass
-
-#a = np.array([1,0,0,0,1,0]).reshape((2,3))
-
-#print(softmax(a))
-
-
 
 
---
+++ MLDemo/test.py
@@ -0,0 +1,15 @@
+
+
+import numpy as np
+
+def sigmod(z):
+	return 1 / (1 + np.exp(-z))
+
+a = np.ones((2,3))
+b = np.array([1,2,3]).reshape((1,3))
+
+j = sigmod(a)*(1-sigmod(a))
+k = sigmod(a).T.dot(1-sigmod(a))
+print(j)
+print(k)
+
