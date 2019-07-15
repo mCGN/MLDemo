0cacba63890e2f1d6761cd5ea3d7f070f930c4f4
CGN <p1398010785@163.com>
2019-07-15 22:56:54 +0800

    bp

--- MLDemo/NewFolder1/bp2.py
+++ MLDemo/NewFolder1/bp2.py
@@ -12,7 +12,7 @@ def dsigmod(a):
 
 def softmax(z):
 	e = np.exp(z)
-	return e / np.sum(e)
+	return e / np.sum(e,axis=1,keepdims=True)
 
 class BP(object):
 	def __init__(self):
@@ -21,6 +21,7 @@ class BP(object):
 		self.m,self.n = self.LL.shape
 		self.x = np.array(self.LL)
 		self.y = np.zeros((self.m,3))
+		self.target = np.array(iris.target)
 		for i in range(self.m):
 			self.y[i][iris.target[i]] = 1
 
@@ -29,41 +30,46 @@ class BP(object):
 		self.b1 = np.zeros((1,3))
 		self.b2 = np.zeros((1,3))
 
-		self.rate = 0.002
+		self.rate = 0.5
 	
 
-	def backPropagation(self,sample,y,p=1):
+	def backPropagation(self,sample,y,p=0):
 		m,n = sample.shape
-		a1 = sigmod(sample.dot(self.w1) + self.b1)
-		a2 = sigmod(a1.dot(self.w2) + self.b2)
+		a1 = sigmod(np.dot(sample,self.w1) + self.b1)
+		a2 = softmax(a1.dot(self.w2) + self.b2)
 
-		l2 = -(y - a2) * dsigmod(a2)
-		l1 = l2.dot(self.w2.T) * dsigmod(a1)
+		loss = np.sum(-np.log(a2[range(self.m),self.target])) / self.m
+		
+		l3 = a2
+		l3[range(self.m),self.target]-=1
+		l3/=self.m
 
-		dw2 = a1.T.dot(l2)
-		db2 = np.sum(l2,axis=0)
+		dw2 = np.dot(a2.T,l3)
+		db2 = np.sum(l3,axis=0,keepdims=True)
 
-		dw1 = sample.T.dot(l1)
-		db1 = np.sum(l1,axis=0)
+		l2 = np.dot(l3,self.w2) * (dsigmod(a1))
+
+
+		dw1 = np.dot(self.x.T,l2)
+		db1 = np.sum(l2,axis=0,keepdims=True)
 
 		self.w2 -= self.rate * dw2
-		self.w1 -= self.rate * dw1
 		self.b2 -= self.rate * db2
+		self.w1 -= self.rate * dw1
 		self.b1 -= self.rate * db1
 
-		if p==0:
-			print (np.sum(1/m*(y-a2)**2))
+		if p == 0:
+			print(loss)
 
 	def predict(self,input):
 		a1 = sigmod(input.dot(self.w1) + self.b1)
-		a2 = sigmod(a1.dot(self.w2) + self.b2)
+		a2 = softmax(a1.dot(self.w2) + self.b2)
 		return a2
 
 bp = BP()
 
-for i in range(1500):
-	for k in range(bp.m):
-		bp.backPropagation(bp.x[k].reshape(1,bp.n),bp.y[k].reshape(1,3),k)
+for i in range(20000):
+	bp.backPropagation(bp.x,bp.y,i%100)
 
 print(bp.w1)
 print(bp.b1)
